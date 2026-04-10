dd9afef464a017dab5e076ead9940263499befc0
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2026-04-10 17:23:57 -0400

    Possibly fix null value?

--- src/platform/psp/psp_dialogs.zig
+++ src/platform/psp/psp_dialogs.zig
@@ -112,12 +112,14 @@ pub fn showNetDialog() bool {
 // ---- On-Screen Keyboard ---------------------------------------------------
 
 pub fn showOSK(description: []const u16, out_text: []u16, max_text_limit: c_int) c_int {
+    var empty_text = [_]u16{0};
     var osk_data = std.mem.zeroes(sdk.c.types.SceUtilityOskData);
     osk_data.language = 0; // PSP_UTILITY_OSK_LANGUAGE_DEFAULT
     osk_data.lines = 1;
     osk_data.unk_24 = 1;
     osk_data.inputtype = 0; // PSP_UTILITY_OSK_INPUTTYPE_ALL
     osk_data.desc = @ptrCast(@constCast(description.ptr));
+    osk_data.intext = @ptrCast(&empty_text);
     osk_data.outtextlength = @intCast(out_text.len);
     osk_data.outtextlimit = max_text_limit;
     osk_data.outtext = @ptrCast(out_text.ptr);
